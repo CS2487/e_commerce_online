5594520fccee3d1e1d3a2d1e99d30f067865a399
Farea AL-Dhela'a <127134204+CS2487@users.noreply.github.com>
2026-01-11 23:37:42 +0300

    Delete StudioProjects directory

--- StudioProjects/bmi_calclator/android/app/src/main/kotlin/com/tests/bmi_calclator/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.tests.bmi_calclator
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- StudioProjects/masrofaty/lib/features/models/chat_message.dart
+++
@@ -1,5 +0,0 @@
-class ChatMessage {
-  final String text;
-  final bool fromUser;
-  ChatMessage(this.text, this.fromUser);
-}
